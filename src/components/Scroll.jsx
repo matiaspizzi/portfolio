5328d036dd0bebdb59d8dfe1a96577192bd4dca7
ron2452 <matias.nahuel.pizzi@hotmail.com>
2022-08-31 19:48:19 -0300

    .

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -18,7 +18,7 @@ const featuredProducts = [
 const Scroll = () => {
   return (
     <div className='flex justify-center py-20'>
-        <div className='bg-[#252525] h-fit md:w-[65%] gap-8 w-full flex snap-x snap-mandatory overflow-x-auto p-4 pb-6 shadow-inner scrollbar-thin scrollbar-thumb-emerald-400 scrollbar-track-slate-700'>
+        <div className='cursor-grab bg-[#252525] h-fit md:w-[65%] gap-8 w-full flex snap-x snap-mandatory overflow-x-auto p-4 pb-6 shadow-inner scrollbar-thin scrollbar-thumb-emerald-400 scrollbar-track-slate-700'>
             {featuredProducts.map((product, index) => (
                 <div className='snap-center shrink-0 first:md:pl-10 '>
                     <img src={product} key={index} alt="" className='shrink-0 w-64 sm:w-72 md:w-96 ' />
